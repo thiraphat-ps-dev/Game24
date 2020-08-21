14d6f0192caa8ef3cd2cd158f4944009a82fc9e5
gun <thiraphat.ps.dev@gmail.com>
2020-08-21 14:24:26 +0700

    [:zap:] Create ServiceAPI search food detail

--- frontend/node/server.js
+++ frontend/node/server.js
@@ -1,17 +1,54 @@
 const express = require('express');
+const {
+  Client,
+  PlaceInputType,
+} = require('@googlemaps/google-maps-services-js');
+const client = new Client({});
+
 const app = express();
 let port = 8000;
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
-  res.setHeader("Access-Control-Allow-Headers", "content-type,x-access-token");
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type,x-access-token');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
 app.get('/', function (req, res) {
   res.send('hello from node js');
+});
+
+app.get('/food', async function (req, res) {
+  const data = ({
+    data: { candidates },
+  } = await client.findPlaceFromText({
+    params: {
+      input: req.query.input,
+      inputtype: PlaceInputType.textQuery,
+      language: 'th',
+      fields: [
+        'name',
+        'opening_hours',
+        'icon',
+        'permanently_closed',
+        'photos',
+        'place_id',
+        'plus_code',
+        'type',
+
+        'rating',
+        'price_level',
+
+        'user_ratings_total',
+      ],
+      key: 'AIzaSyD4W8rIBYcH70TtOSTtgqQJgK2zjBe4-P8',
+      radius: '80000',
+    },
+  }));
+
+  res.json(data.data);
 });
 
 app.get('/game24', function (req, res) {
